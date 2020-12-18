cefb062cb122bc2b8248bb00f02b5d4cc19295e1
alex02 <alexemilov1@gmail.com>
2020-12-18 19:09:40 +0200

    some updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-function ade(a,b,c){return a.attachEvent?a.attachEvent("on"+b,c):a.addEventListener(b,c,!1)}document.addEventListener("DOMContentLoaded",function(){ade(document.getElementById("mobile-menu"),"click",function(){this.classList.toggle("opened");this.setAttribute("aria-expanded",this.classList.contains("opened"));document.getElementById("home-menu").classList.toggle("opened")})});
+document.addEventListener("DOMContentLoaded", function(event)
+{
+   var me = document.getElementById('mobile-menu'),
+       he = document.getElementById('home-menu'),
+       f = function()
+         {
+          this.classList.toggle('opened');
+          this.setAttribute('aria-expanded', this.classList.contains('opened'));
+          he.classList.toggle('opened');
+         };
+         
+   return me.addEventListener('click', f, false);
+});
